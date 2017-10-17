8c557ce55e24d189af5c13225cc2879235465804
Hack Reactor Student <student@hackreactor.com>
2017-10-16 22:19:22 -0700

    completed beesbeesbees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,18 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.honeyPot = 0;
+  this.canFly = true;
+  this.treasureChest = [];
+ 
 };
+
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+ForagerBee.prototype.forage = function (value) {
+  this.treasureChest.push(value);
+};
+
 
